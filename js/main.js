0bb15f144b0e8e31253b9d0e68fd23e69b20e427
Lucas Miguez <lucasmiguez46@gmail.com>
2024-05-29 23:32:24 -0300

    js incorporado con funciones

--- js/main.js
+++ js/main.js
@@ -1,2 +1,22 @@
 //esto es un comentario
 // alert("hola mundo!");
+
+function validarEdadYNombre() {
+    let edad = document.getElementById("edad").value;
+    let nombre = document.getElementById("nombre").value;
+
+    console.log("Tu nombre es: " + nombre + ", " + "Tu edad es: " + edad);
+    alert("Tu nombre es: " + nombre + ", " + "Tu edad es: " + edad)
+}
+
+//  function validarEdad() {
+//      let edad = document.getElementById("edad").value;
+//      console.log("Tu edad es: " + edad);
+//      alert("Tu edad es: " + edad)
+//  }
+
+// function validarNombre(){
+//     let nombre = document.getElementById("nombre").value;
+//     console.log("Tu nombre es: " + nombre);
+//     alert("Tu nombre es: " + nombre)
+// }
