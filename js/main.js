4843f0af0c8663789318810533e9d95c5ef8b1e3
Lucas Miguez <lucasmiguez46@gmail.com>
2024-06-04 21:12:13 -0300

    ingresos de if

--- js/main.js
+++ js/main.js
@@ -46,12 +46,36 @@ function validarEdadYNombre() {
 // let salida= "buenas: " + nombre + " que tenga un buen dia";
 // alert(salida);
 
-let nombre=prompt("ingrese su nombre y apellido: ");
-let estatura= parseFloat(prompt("ingrese la estatura: "));
-let peso=parseFloat(prompt("ingrese su peso: "));
+// let nombre=prompt("ingrese su nombre y apellido: ");
+// let estatura= parseFloat(prompt("ingrese la estatura: "));
+// let peso=parseFloat(prompt("ingrese su peso: "));
 
-let alturaAlCuadrado = estatura * estatura;
+// let alturaAlCuadrado = estatura * estatura;
 
-let salida = peso / alturaAlCuadrado;
+// let salida = peso / alturaAlCuadrado;
 
-alert("buenas " + nombre + "tu IMC es: " + salida.toFixed(2));
+// alert("buenas " + nombre + "tu IMC es: " + salida.toFixed(2));
+
+
+//ejemplo if
+// let edad=27;
+// let edad = prompt("Por favor ingresar edad: ");
+
+//  if (18<= edad && edad <= 23) {
+//      console.log("eres mayor de edad");
+//  }else if(edad >=24){
+//      console.log("eres un adulto");
+//  }else{
+//      console.log("eres menor de edad");
+//  }
+
+let producto1 = parseFloat(prompt("Ingrese el importe del producto1: "));
+let producto2 = parseFloat(prompt("Ingrese el importe del producto2: "));
+
+let total = producto1 + producto2;
+
+if (total > 10000) {
+    total = total * 0.9;
+} 
+
+alert("el total a pagar es: " + total);
