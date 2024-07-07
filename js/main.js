ca8ab9ff7a7ed1a6a8c9f95226ffb20c24d2fbec
Lucas Miguez <lucasmiguez46@gmail.com>
2024-07-06 21:59:49 -0300

    incorporacion de constructores y funciones para el proyecto final

--- js/main.js
+++ js/main.js
@@ -1,181 +1,94 @@
 
-//constructor basico
-class Persona {
-    constructor(nombre, edad, dni, tieneTarjeta){
-    this.nombre = nombre;
-    this.edad = edad;
-    this.dni = dni;
-    this.tieneTarjeta = tieneTarjeta;
+//Para el futuro proyecto final se piensa en una simulacion de compra de helados
+//Inicio el programa estableciendo ciertos arrays
+const arrayPedidos=[]; //establezco una lista vacia para futuros pedidos.
+const arrayProductosPaleta=[]; //establezco una lista de productos actuales de la empresa de helados en base a paletas.
+const arrayProductosPote=[]; //establezco una lista de productos actuales de la empresa de helados en base a potes.
+
+class Productos{
+    constructor(productos){
+        this.items=productos;
     }
-    //funciones dentro de constructores
-    hablar(){ 
-        console.log("HOLA SOY "+ this.nombre)
+
+    agregarProducto(nombreEntrada, precioEntrada) {
+        const producto = {id:this.generarId(), nombre:nombreEntrada, precio:precioEntrada}; // Crear un nuevo objeto Producto
+        this.items.push(producto); // Agregando al Array items de la Clase Producto, el nuevo objeto Producto creado
+        console.log("Producto agregado!");
     }
-    
+
+    buscarProducto(id) {
+        return this.items.find(item => item.id == id)
+    }
+
+    obtenerProductos() {
+        return this.items;
+    }
+
+    listarProductos() {
+        let salida = "";
+
+        for (const producto of this.items) {
+            salida += `${producto.id} - ${producto.nombre} $${producto.precio}\n`;
+        }
+
+        alert(salida);
+    }
+
+    totalProductos() {
+        return this.items.length;
+    }
+
+    generarId() {
+        let max = 0;
+
+        this.items.forEach(item => {
+            if (item.id > max) {
+                max = item.id;
+            }
+        });
+
+        return max + 1;
+    }
+
+    eliminarProducto(id) {
+        // Opción #1 - Eliminar con método splice
+        let pos = this.items.findIndex(item => item.id == id); // Busqué la posición del elemento a eliminar en el array items (2)
+        this.items.splice(pos, 1); // Eliminar el elemento de la posición "pos"
+        
+        // Opción #2 - Eliminar con método filter
+        //this.items = this.items.filter(item => item.id != id); // Eliminar el elemento de la posición "pos"
+        
+        console.log("Se eliminó el Producto: #" + id);
+        this.items.forEach(item => {item.id = item.id -1})
+        
+    }
 }
 
-//creando nuevo humano en base al constructor
-const humano1 = new Persona("Carlos", 24, 42297696, false);
-const humano2 = new Persona("Ana", 20, 42368184, true);
-const humano3 = new Persona("Alejandro", 19, 42727374, true);
-const humano4 = new Persona("Zoe", 22, 42277239, false);
+//creacion de los objetos arrays.
+const catalogoPaleta = new Productos(arrayProductosPaleta);
+const catalogoPote = new Productos(arrayProductosPote);
 
-//usando funcion dentro del constructor para que haga algo
-// humano1.hablar();
-// humano2.hablar();
-// humano3.hablar();
-// humano4.hablar();
+//comienzo a agregar contenido al catalogo de paletas
+catalogoPaleta.agregarProducto("bombom helado", 1500);
+catalogoPaleta.agregarProducto("helado de agua", 1200);
+catalogoPaleta.agregarProducto("vainilla ", 1500);
 
-const array = [humano1.nombre, humano2.nombre, humano3.nombre, humano4.nombre];
+//comienzo a agregar contenido al catalogo de potes
+catalogoPote.agregarProducto("Pote de 1/4", 2000);
+catalogoPote.agregarProducto("Pote de 1/2", 4000);
+catalogoPote.agregarProducto("Pote de 1 kilo", 6000);
 
-console.log(array);
+console.log(catalogoPaleta.obtenerProductos());
+console.log(catalogoPote.obtenerProductos());
 
-const numeros = [1, 2, 3, 4, 5];
-for (let index = 0; index < numeros.length; index++) {
-    alert(numeros[index]);
-}
+// catalogo.agregarProducto("Cunnington Cola", 1900);
+// catalogo.agregarProducto("Sprite", 1800);
+// catalogo.agregarProducto("Pretty", 1400);
+// console.log(catalogo.obtenerProductos());
 
-//===================================
-//no se me ocurrio algo mas original y como debia demostrar que habia entendido realize lo siguiente.
-//Este es un simulador de seguros(basico).
-// let nombre = prompt("Este es un simulador de seguros.\nPor favor colocar su nombre: \n(Coloque ESC para terminar)");
+//console.log(catalogo.eliminarProducto(1));
 
-// while (nombre != "ESC"){
-//     //primero, la marca del auto.
-//     let textoMarca = "ingrese con el numero el tipo de marca del auto: \n\n";
-//     textoMarca += "1-Ford\n";
-//     textoMarca += "2-Chevrolet\n";
-//     textoMarca += "3-Audi\n";
-//     textoMarca += "4-Fiat";
-
-//     let marca = parseInt(prompt(textoMarca));
-    
-//     //segundo, pido el año del vehiculo.
-//     let textoAnio = "ingrese con el numero el año aproximado del vehiculo: \n\n";
-//     textoAnio += "1-2000 al 2005\n";
-//     textoAnio += "2-2006 al 2010\n";
-//     textoAnio += "3-2011 al 2015\n";
-//     textoAnio += "4-2016 al 2020\n";
-//     textoAnio += "5-2021 al 2025";
-
-//     let anio = parseInt(prompt(textoAnio));
-
-//     //tercero, pregunto si el auto es 0km o no.
-//     let km = prompt("¿su auto es 0km?\n" + "coloque (si) si su auto es 0 kilometros o (no) si no lo es: ");
-
-//     //Cuarto, pregunto si el auto utiliza GNC o no.
-//     let gnc = prompt("¿su auto utiliza GNC?\n" + "coloque (si) si su auto lo utiliza o (no) si no lo hace: ");
-
-//     //estas son variables de valor en referencia al valor numero de dinero
-//     let valorMarca;
-//     let valorAnio;
-
-//     //obtencion de marca y año String
-//     marca = obtenerMarca(marca);
-//     anio = obtenerAnioAproximado(anio);
-
-//     //obtencion de los valores de marca y año interger
-//     valorMarca = obtenerValorMarca(marca);
-//     valorAnio = obtenerValorAnio(anio);
-
-//     //calculos finales y verficaciones
-//     let valorTotal = sumar(valorMarca,valorAnio);
-//     valorTotal = verificacionKm(valorTotal,km);
-//     valorTotal = verificacionGnc(valorTotal,gnc);
-
-//     // SALIDA DE DATOS
-//     salida(marca,anio,km,gnc,nombre,valorMarca,valorAnio,valorTotal);
-//     nombre = prompt("Este es un simulador de seguros.\nPor favor colocar su nombre: \n(Coloque ESC para terminar)");
-// }
-
-// //Desde aca empiezan las funciones
-// function obtenerMarca(numero) {
-//     switch (numero) {
-//         case 1:
-//             return "Ford";
-//         case 2:
-//             return "Chevrolet";
-//         case 3:
-//             return "Audi";
-//         case 4:
-//             return "Fiat";
-//         default:
-//             return undefined;
-//     }
-// }
-
-// function obtenerAnioAproximado(numero) {
-//     switch (numero) {
-//         case 1:
-//             return "2000 al 2005";
-//         case 2:
-//             return "2006 al 2010";
-//         case 3:
-//             return "2011 al 2015";
-//         case 4:
-//             return "2016 al 2020";
-//         case 5:
-//             return "2021 al 2025";
-//         default:
-//             return undefined;
-//     }
-// }
-
-// function obtenerValorMarca(marca) {
-//     if (marca == "Ford") {
-//         return 10000;
-//     }else if (marca == "Chevrolet") {
-//         return 12000;
-//     }else if (marca == "Audi") {
-//         return 14000;
-//     }else if (marca == "Fiat") {
-//         return 16000;
-//     }else{
-//         return undefined;
-//     }
-// }
-
-// function obtenerValorAnio(anio){
-//     if (anio == "2000 al 2005") {
-//         return 10000;
-//     }else if (anio == "2006 al 2010") {
-//         return 12000;
-//     }else if (anio == "2011 al 2015") {
-//         return 14000;
-//     }else if (anio == "2016 al 2020") {
-//         return 16000;
-//     }else if (anio == "2021 al 2025") {
-//         return 18000;
-//     }else{
-//         return undefined;
-//     }
-// }
-
-// function sumar(valor1,valor2){
-//     return valor1 + valor2;
-// }
-
-// function verificacionKm(valorTotal,km) {
-//     if (km=="si") {
-//         valorTotal=valorTotal+((valorTotal*10)/100);
-//     }
-//     return valorTotal;
-// }
-
-// function verificacionGnc(valorTotal,gnc){
-//     if (gnc=="si") {
-//         valorTotal=valorTotal+((valorTotal*30)/100);
-//     }
-//     return valorTotal;
-// }
-
-// function salida(marca, anio,km,gnc, nombre, valorMarca,valorAnio,valorTotal) {
-//     let aviso= "su auto es de la marca: " + marca +"\n";
-//     aviso += "del año aproximado: " + anio + "\n";
-//     aviso += "¿es 0km?: " + km +"\n";
-//     aviso += "¿tiene gnc?: " + gnc +"\n";
-//     aviso += "valor por marca: " + valorMarca + "\n" + "valor por año: "  + valorAnio + "\n" + "valor total por el seguro: " + valorTotal;
-
-//     alert("Hola " + nombre + ", tu informacion es la siguiente: \n" + aviso);
-// }
-//===================================
+// const pedidosDelUsuario = new Productos(arrayPedidos);
+// console.log(pedidosDelUsuario.agregarProducto(catalogo.buscarProducto(1).nombre,catalogo.buscarProducto(1).precio));
+// console.log(pedidosDelUsuario.agregarProducto(catalogo.buscarProducto(1).nombre,catalogo.buscarProducto(1).precio));
+// console.log(pedidosDelUsuario.obtenerProductos());
