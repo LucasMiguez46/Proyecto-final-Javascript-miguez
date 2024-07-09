5179d652d133f75334e8fc0ddc97a4979ebf942a
Lucas Miguez <lucasmiguez46@gmail.com>
2024-07-09 14:55:21 -0300

    codigo entregable2 terminado, seguir revisando

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ const arrayPedidos=[]; //establezco una lista vacia para futuros pedidos.
 const arrayProductosPaleta=[]; //establezco una lista de productos actuales de la empresa de helados en base a paletas.
 const arrayProductosPote=[]; //establezco una lista de productos actuales de la empresa de helados en base a potes.
 
+//====Inicio de la clase Productos con su constructor y funciones propias====//
 class Productos{
     constructor(productos){
         this.items=productos;
@@ -13,7 +14,7 @@ class Productos{
     agregarProducto(nombreEntrada, precioEntrada) {
         const producto = {id:this.generarId(), nombre:nombreEntrada, precio:precioEntrada}; // Crear un nuevo objeto Producto
         this.items.push(producto); // Agregando al Array items de la Clase Producto, el nuevo objeto Producto creado
-        console.log("Producto agregado!");
+        //console.log("Producto agregado!");
     }
 
     buscarProducto(id) {
@@ -55,9 +56,6 @@ class Productos{
         let pos = this.items.findIndex(item => item.id == id); // Busqué la posición del elemento a eliminar en el array items (2)
         this.items.splice(pos, 1); // Eliminar el elemento de la posición "pos"
         
-        // Opción #2 - Eliminar con método filter
-        //this.items = this.items.filter(item => item.id != id); // Eliminar el elemento de la posición "pos"
-        
         console.log("Se eliminó el Producto: #" + id);
         this.items.forEach(item => {item.id = item.id -1})
         
@@ -77,7 +75,9 @@ class Productos{
         return precio += precio * 0.21;
     }
 }
+//====Fin de la clase Productos con su constructor y funciones propias====//
 
+//====Inicio de variables y funciones que establecen la listas por separado de cada catalogo====//
 //creacion de los objetos arrays.
 const catalogoPaleta = new Productos(arrayProductosPaleta);
 const catalogoPote = new Productos(arrayProductosPote);
@@ -92,30 +92,6 @@ catalogoPote.agregarProducto("Pote de 1/4", 2000);
 catalogoPote.agregarProducto("Pote de 1/2", 4000);
 catalogoPote.agregarProducto("Pote de 1 kilo", 6000);
 
-//verificacion por consola que este todo bien.
-//console.log(catalogoPaleta.obtenerProductos());
-//console.log(catalogoPote.obtenerProductos());
-
-//obtenemos la opcion que quiera el usuario y su nombre.
-const nombre = prompt("Ingrese su nombre por favor: ");
-let textOpcion = "Escoja la opcion numeral que quiera: \n";
-textOpcion += "1-Paletas\n";
-textOpcion += "2-Pote de helado\n";
-
-//la opcion se guardara en esta variable.
-let opcion=parseInt(prompt(textOpcion))
-
-//tal como su nombre lo indica, paso la opcion numeral a String.
-opcion = pasaAString(opcion);
-
-function pasaAString(op) {
-    if (op == 1) {
-        return "paletas";
-    }else{
-        return "potes";
-    }
-}
-
 //establezco cadenas para mostrar los productos dependiendo la opcion que se elija antes.
 let textCatalogoPaleta = "Escoja la opcion numeral que quiera: \n";
 textCatalogoPaleta += "1-" + catalogoPaleta.buscarProducto(1).nombre + "precio: " + catalogoPaleta.buscarProducto(1).precio + ".\n";
@@ -127,7 +103,53 @@ textCatalogoPote += "1-" + catalogoPote.buscarProducto(1).nombre + "precio: " + 
 textCatalogoPote += "2-" + catalogoPote.buscarProducto(2).nombre + "precio: " + catalogoPote.buscarProducto(2).precio + ".\n";
 textCatalogoPote += "3-" + catalogoPote.buscarProducto(3).nombre + "precio: " + catalogoPote.buscarProducto(3).precio + ".\n";
 
-//VERIFICAR
+//lista del usuario
+const pedidosDelUsuario = new Productos(arrayPedidos);
+//====Fin de variables y funciones que establecen la listas por separado de cada catalogo====//
+
+//antes del ciclo preguntar confirmacion y nombre
+const nombre = prompt("Ingrese su nombre por favor: ");
+let continuar=true;
+
+//====Inicio del ciclo====//
+do{
+    //obtenemos la opcion que quiera el usuario.
+    let textOpcion = "Escoja la opcion numeral que quiera: \n";
+    textOpcion += "1-Paletas\n";
+    textOpcion += "2-Pote de helado\n";
+
+    //la opcion se guardara en esta variable.
+    let opcion=parseInt(prompt(textOpcion))
+
+    //tal como su nombre lo indica, paso la opcion numeral a String.
+    opcion = pasaAString(opcion);
+
+    //guardo la opcion a pedir en la siguiente variable.
+    let opcionAPedir= eleccion(opcion);
+
+    //desde aca comienza el codigo que afecta a la lista de pedidos del usuario.
+    let productoParaLista = agregarAListaUsuario(opcion, opcionAPedir)
+
+    continuar = confirm("¿Desea hacer otro pedido?");
+}while(continuar);
+//====Fin del ciclo====//
+
+//variables que guardaran y realizaran pequeños calculos como suma de productos y incorporacion del iva
+let precioTotal = pedidosDelUsuario.sumarPrecios();
+precioTotal = pedidosDelUsuario.sumarIva(precioTotal);
+
+//muestra por consola las opciones elegidas.
+salida(nombre,pedidosDelUsuario,precioTotal);
+
+//====Inicio de funciones fuera del ciclo====//
+function pasaAString(op) {
+    if (op == 1) {
+        return "paletas";
+    }else{
+        return "potes";
+    }
+}
+
 //en efecto, la funcion muestra por pantalla emergente las posibilidades dependiendo de la eleccion anterior.
 function eleccion(string) {
     if (string == "paletas") {
@@ -137,33 +159,19 @@ function eleccion(string) {
     }
 }
 
-//VERIFICAR
-//guardo la opcion a pedir en la siguiente variable.
-let opcionAPedir= eleccion(opcion);
-
-
-//si me guarda el numero, seguir verificando.
-console.log("opcion a pedir: "+opcionAPedir);
-//desde aca comienza el codigo que afecta a la lista de pedidos del usuario.
-const pedidosDelUsuario = new Productos(arrayPedidos);
-
-if (opcion == "paletas") {
-    pedidosDelUsuario.agregarProducto(catalogoPaleta.buscarProducto(opcionAPedir).nombre,catalogoPaleta.buscarProducto(opcionAPedir).precio);
-}else{
-    pedidosDelUsuario.agregarProducto(catalogoPote.buscarProducto(opcionAPedir).nombre,catalogoPote.buscarProducto(opcionAPedir).precio);
+//funcion que toma el producto dado su ID y lo agrega a la lista del usuario
+function agregarAListaUsuario(opcion, numID) {
+    if (opcion == "paletas") {
+        pedidosDelUsuario.agregarProducto(catalogoPaleta.buscarProducto(numID).nombre,catalogoPaleta.buscarProducto(numID).precio);
+    }else{
+        pedidosDelUsuario.agregarProducto(catalogoPote.buscarProducto(numID).nombre,catalogoPote.buscarProducto(numID).precio);
+    }
 }
-// pedidosDelUsuario.agregarProducto(catalogoPaleta.buscarProducto(1).nombre,catalogoPaleta.buscarProducto(1).precio)
-// pedidosDelUsuario.agregarProducto(catalogoPaleta.buscarProducto(3).nombre,catalogoPaleta.buscarProducto(3).precio)
-// pedidosDelUsuario.agregarProducto(catalogoPote.buscarProducto(3).nombre,catalogoPote.buscarProducto(3).precio)
-let precioTotal = pedidosDelUsuario.sumarPrecios();
-precioTotal = pedidosDelUsuario.sumarIva(precioTotal);
 
-//muestra por consola las opciones elegidas.
-alert("Nombre:" + nombre + ", opcion: " + opcion + " y la lista de productos es: ");
-pedidosDelUsuario.listarProductos();
-alert("el precio total es el siguiente: " + precioTotal);
-
-// const pedidosDelUsuario = new Productos(arrayPedidos);
-// console.log(pedidosDelUsuario.agregarProducto(catalogo.buscarProducto(1).nombre,catalogo.buscarProducto(1).precio));
-// console.log(pedidosDelUsuario.agregarProducto(catalogo.buscarProducto(1).nombre,catalogo.buscarProducto(1).precio));
-// console.log(pedidosDelUsuario.obtenerProductos());
+//funcion utilizada para la salida de informacion final
+function salida(nombre, listaDePedidos, precioTotal) {
+    alert("Nombre: " + nombre + " y la lista de productos es: ");
+    listaDePedidos.listarProductos();
+    alert("el precio total es el siguiente: " + precioTotal);
+}
+//====Fin de funciones fuera del ciclo====//
